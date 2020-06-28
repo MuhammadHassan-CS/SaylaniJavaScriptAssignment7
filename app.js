7381fe6e0072a9dbbe23d7c20614980bfd4ee354
MuhammadHassan-CS <muhammadhusan345@gmail.com>
2020-06-28 22:10:22 +0500

    SaylaniJavaScript Assignment7 completed

---
+++ app.js
@@ -0,0 +1,34 @@
+// function foo(){
+//     alert('Hello Hi')
+// }
+
+
+// function getName(){
+//     var name = document.getElementById('xyz');
+//     console.log(name.value)
+
+//     name.value = '';
+// }
+
+
+function getNumber(num){
+    console.log(num)
+    var result = document.getElementById('result')
+
+    result.value += num;
+}
+
+function clearResult(){
+    var result = document.getElementById('result')
+
+    result.value = '';
+}
+
+
+function getResult(){
+    var result = document.getElementById('result')
+   
+    result.value = eval(result.value)
+
+
+}
